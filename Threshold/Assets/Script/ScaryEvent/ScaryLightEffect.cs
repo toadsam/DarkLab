8d3b08a7e9537bd2aa88278856081b0ef8af772f
toadsam <75034052+toadsam@users.noreply.github.com>
2024-04-24 21:08:25 +0900

    [ADD] 에디터 스크립트 추가 + 이벤트 이펙트 수정 (재훈)
    Co-Authored-By: hyeona <90515062+BaeHyeonA@users.noreply.github.com>

--- Threshold/Assets/Script/ScaryEvent/ScaryLightEffect.cs
+++ Threshold/Assets/Script/ScaryEvent/ScaryLightEffect.cs
@@ -3,20 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class ScaryLightEffect : ScaryEffect
+public enum LightEffectType
 {
-    // 대상이 되는 라이트
-    public Light targetLight;
+    None,
+    Flicker,
+    ColorChange,
+    IntensityChange
+}
 
-    // 대상 설정 값
+public class ScaryLightEffect : MonoBehaviour
+{
+    public LightEffectType effectType;
+    public Light lightComponent; //이거 왜 필요한거야?? target에서 가져오지 않나??!
     public Color targetColor;
     public float targetIntensity;
     public float targetIndirectMultiplier;
     public LightShadows targetShadowType;
     public bool targetDrawHalo;
+    public float duration;
 
-    public void ColorOff()
+    public ScaryEvent targetSource;
+
+    //Light도 DoTween 이용해서 쉽게 구현할건지
+    //아니면 코루틴 이용해서 구현할건지 정해야할 것 같습니다!!!
+    //근데 코드가 깔끔한거는 DoTween인 것 같아요,,훗,,
+
+    void Start()
     {
-        this.GetComponent<Light>().intensity = 10;
+        targetSource = transform.parent.GetComponent<ScaryEvent>();
     }
+    
+    public void Flicker()
+    {
+        var a = targetSource.GetCurrentTarget<Light>("light");
+        DOTween.To(() => a.intensity, x => a.intensity = x, targetIntensity, duration)
+        .SetEase(Ease.InOutQuad)
+        .SetLoops(-1, LoopType.Yoyo)
+        .SetDelay(0.5f);
+    }
+
+    public void ColorChange()
+    {
+        var a = targetSource.GetCurrentTarget<Light>("light");
+        a.DOColor(targetColor, duration);
+    }
+
+    //DoTween 이용
+    public void IntensityChange()
+    {
+        var a = targetSource.GetCurrentTarget<Light>("light");
+        a.DOIntensity(targetIntensity, duration)
+        .SetEase(Ease.InOutSine);
+    }
+
+    
 }
