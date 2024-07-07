ee0744315fb5d3586f130e5009e423e2b252267e
toadsam <75034052+toadsam@users.noreply.github.com>
2024-07-07 21:30:42 +0900

    [ADD] 눈 감기 상호작용을 위한 추가 작업
    Co-Authored-By: hyeona <90515062+BaeHyeonA@users.noreply.github.com>

--- Threshold/Assets/Scripts/Camera/CameraChange.cs
+++ Threshold/Assets/Scripts/Camera/CameraChange.cs
@@ -2,25 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Rendering;
 
 public class CameraChange : MonoBehaviour
 {
     private int currentCameraIndex = 0;
 
     public CinemachineVirtualCamera[] cameras;
+    
+    public Volume volume;
 
 	void Update()
     {
-        if (!PlayerInteraction.focusInteraction)
-        {
+        
             //Q 키를 누르면 왼쪽 카메라 활성화
             if (Input.GetKeyDown(KeyCode.Q))
                 SwitchCamera((currentCameraIndex - 1 + cameras.Length) % cameras.Length);
 
             //E 키를 누르면 오른쪽 카메라 활성화
             if (Input.GetKeyDown(KeyCode.E))
+                if (PlayerInteraction.focusInteraction)
+                {
+                    Debug.Log("여기에 들어오면 눈감기");
+                }
+                else
+                {
                 SwitchCamera((currentCameraIndex + 1) % cameras.Length);
-        }
+                }
+        
     }
 
     void SwitchCamera(int newIndex)
@@ -30,4 +39,12 @@ public class CameraChange : MonoBehaviour
 
         currentCameraIndex = newIndex;
     }
+
+    void CloseEyesMode()
+    {
+        Debug.Log("여기에 들어오면 눈감기");
+        
+    }
+    
+    
 }//여기 코드에다가 focus mode가 되어 있을 때랑 아닐 때랑 구분해서 넣기
