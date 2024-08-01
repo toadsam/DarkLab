50cfa9ed35665b09f74530df9c685bacbbbbfd21
PAVDUP <kimhwinyen@gmail.com>
2024-08-01 22:43:14 +0900

    [ADD] done with title and main ui and those functions

--- Threshold/Assets/Scripts/UI/Test.cs
+++ Threshold/Assets/Scripts/UI/Test.cs
@@ -7,6 +7,12 @@ public class Test : MonoBehaviour
 {
     public void gameStart()
     {
-        SceneManager.LoadScene("BathRoom");
+        SceneManager.LoadScene("MainScene");
+        ProgressChecker.Instance.StartGameSession(); // progress checker 를 활성화 (singleton이라 부르면 활성화됨.)
+    }
+    
+    public void gameEnd()
+    {
+        Application.Quit();
     }
 }
