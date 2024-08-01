281dea53a79a8c387a3699aabf28e5ba0ce54e7d
PAVDUP <kimhwinyen@gmail.com>
2024-08-01 13:54:03 +0900

    [MINOR] Change ScaryEvent for assign tier in event

--- Threshold/Assets/Scripts/Editor/ScaryEventEditor.cs
+++ Threshold/Assets/Scripts/Editor/ScaryEventEditor.cs
@@ -1,30 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using ScaryEvents;
-using UnityEngine;
-using UnityEngine.Events;
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(ScaryEvent))]
 public class ScaryEventEditor : Editor
 {
     private bool showOnStartEvents = false;
+    private SerializedProperty metaDataProperty;
+    private SerializedProperty tierProperty;
+
+    private void OnEnable()
+    {
+        metaDataProperty = serializedObject.FindProperty("metaData");
+        tierProperty = metaDataProperty.FindPropertyRelative("tier");
+    }
+
     public override void OnInspectorGUI()
     {
         ScaryEvent script = (ScaryEvent)target;
         serializedObject.Update();
-
         
-        script.metaData.tier = (scaryEventTier)EditorGUILayout.EnumPopup("Scary Event Tier", script.metaData.tier);
-
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(tierProperty, new GUIContent("Scary Event Tier"));
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+        }
         
         EditorGUILayout.ObjectField("Current Event Target", script.currentEventTarget, typeof(ObjectInfoHolder), true);
-
         
         showOnStartEvents = EditorGUILayout.Foldout(showOnStartEvents, "On Start Events");
         if (showOnStartEvents)
             EditorGUILayout.PropertyField(serializedObject.FindProperty("onStart"), new GUIContent("On Start"));
-
+        
         serializedObject.ApplyModifiedProperties();
     }
 }
