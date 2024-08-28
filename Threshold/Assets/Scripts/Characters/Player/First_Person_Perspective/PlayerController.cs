05e5ddfe3641eaf661e691bed99a4bbe4ab46c33
toadsam <toadsam@naver.com>
2024-08-28 15:21:33 +0900

    [ADD] 플레이어가 움직이는 동안 움직임 막는 거 수정 (-재훈)

--- Threshold/Assets/Scripts/Characters/Player/First_Person_Perspective/PlayerController.cs
+++ Threshold/Assets/Scripts/Characters/Player/First_Person_Perspective/PlayerController.cs
@@ -50,7 +50,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(playerEventOff)
+        if(playerEventOff && WakeUp.isWakeUp)
             Move();
       
     }
